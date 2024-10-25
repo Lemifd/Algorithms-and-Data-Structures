7f04d5ae7a64a49c5eca145a229c3def7e462a63
Lemi Fedasa <lemifedasa0@gmail.com>
2024-10-25 22:10:15 +0300

    update name

--- 0926.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-  def minFlipsMonoIncr(self, s: str) -> int:
-    dp = [0] * 2
-
-    for c in s:
-      dp[0], dp[1] = dp[0] + (c == '1'), min(dp[0], dp[1]) + (c == '0')
-
-    return min(dp)
